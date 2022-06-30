e9ca8f31e6f4d5365bbbdfacfaa2f45fd2610087
valere92 <romualdvalerendiguintamack@gmail.com>
2022-06-30 11:18:06 -0700

    5. Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n"); 
+puts("\"Programming is like building a multilingual puzzle"); 
 return (0);
 }
