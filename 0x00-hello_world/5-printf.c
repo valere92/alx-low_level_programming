c1100c15c9da23e60e075180d0a3257cba744ce5
valere92 <romualdvalerendiguintamack@gmail.com>
2022-06-30 10:49:35 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
- *this function pint a message
+ *this function print a message
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+/**
+ * return 0 if is success
+ */
