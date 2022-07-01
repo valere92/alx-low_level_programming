947b55f913d00493f2c5aed5761eb3eeabf1ec53
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-01 16:03:03 -0700

    2. I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 int n;
-unsigned int last_digit;
+int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 last_digit = n%10;
@@ -17,7 +17,7 @@ if (last_digit > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 if (last_digit == 0)
 printf("Last digit of %d is %d and is 0\n", n, last_digit);
-if (last_digit > 0 && last_digit < 6)
+if (last_digit > 0 &&  last_digit < 6)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - inter point
+ * Description: print alphabet in uppercase
+ * Return: 0 as success
+ */
+int main(void)
+{
+for (int i = 97 ; i < 123 ; i++)
+putchar(i);
+return (0);
+}
