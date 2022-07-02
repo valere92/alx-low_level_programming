b46bec6b139ccce7f12892986f2edd7f68f6c693
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-01 20:42:25 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - inter point
+ * Description: print single digit, use putchar
+ * Return: 0 as success
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+putchar(i);
+putchar(10);
+return (0);
+}
