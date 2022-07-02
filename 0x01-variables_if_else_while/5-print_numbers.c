d9854fac229b496e89ca9ade2965b526dcf6a5a1
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-01 20:30:11 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - inter point
+ * Description: print single digit
+ * Return: 0 as sucess
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+printf("%d", i);
+putchar(10);
+return (0);
+}
