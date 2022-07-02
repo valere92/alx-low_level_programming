eaa5c3798f8d99aba380fc2c6145962c05b07094
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-01 23:48:05 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - inter point
+ * Description: reverse alphabet
+ * Return: 0 as success
+ */
+int main(void)
+{
+int i;
+for (i = 122; i >= 97; i--)
+putchar(i);
+putchar(10);
+return (0);
+}
