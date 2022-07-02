f2b55b19f3314fb39cfede1e0f02850ca30dbfe4
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-02 00:56:52 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - inter point
+ * Description: lower case in hexadecimal
+ * Return: 0 as success
+ */
+int main(void)
+{
+int i;
+int ch;
+for (i = 48; i <= 57; i++)
+putchar(i);
+for (ch = 97; ch <= 102; ch++)
+putchar(ch);
+putchar(10);
+return (0);
+}
