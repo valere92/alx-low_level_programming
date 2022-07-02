663a2be7810ff5476cdb10627c2e23774f31b69f
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-02 02:16:34 -0700

    9. Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * amin - inter point
+ * Description: print combination of numbers
+ * Return: 0 as success
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+putchar(44);
+putchar(32);
+}
+putchar(10);
+return (0);
+}
