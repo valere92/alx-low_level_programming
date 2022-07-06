e18f3afb432d53994f1242902977f665ad274816
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-06 15:30:15 +0000

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * _isalpha - chech alphabetic caracters
+ * @c : the decimal number of the char
+ * Return: 1 if is alphabetic 0 if other
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) && (c >= 65 && c <= 90))
+return (1);
+
+return (0);
+}
