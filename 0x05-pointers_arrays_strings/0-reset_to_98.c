6f9c2ac19d6183199e7a0dcc10912a34c26af81b
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-11 10:47:36 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *reset_to_98 - reset the value of an variable to 98
+  *@*n : pionter to an int n
+  *
+  *Return: return noyhing
+  */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
