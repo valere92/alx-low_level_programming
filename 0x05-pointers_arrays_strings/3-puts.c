b78b2ddb82dedbed7b8eeb3fc68cedc141119026
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-11 16:32:40 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -17,3 +17,26 @@ void _puts(char *str)
 	}
 	_putchar('\n');
 }
+
+
+/**
+  *_strlen - return the length of a string
+  *@s : the string parameter
+  *
+  *Return: size of a string which is the length of string
+  */
+
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
