b306766215061164f2410ff9b43a032e0ab2502b
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-11 19:47:17 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *_strcpy - an function
+  *@dest : a parameter
+  *@src : an other parameter
+  *
+  *Return: nothing
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = &src;
+	return (dest);
+}
