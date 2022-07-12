354245ef36f83bed23d6cc8e2cd945e3cb9513ae
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-11 17:24:28 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,15 +10,14 @@
 void rev_string(char *s)
 {
 	int i;
-	
+	char str[];
 
 	for (i = 0; i < _strlen(s); i++)
 	{
-		s[i] = s[_strlen(s) - i];
+		str[i] = s[_strlen(s) - i];
 	}
-	
+	s = str;
 }
-
 
 /**
   *_strlen - return the length of a string
