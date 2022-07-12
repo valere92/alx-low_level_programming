c6bea808ebcbf85d2a88b0fc1ac754ae07c8b11f
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-11 17:03:52 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+  *rev_string - print a string on stdout
+  *@s : the sting to print
+  *
+  *Return: nothing
+  */
+
+void rev_string(char *s)
+{
+	int i;
+	char str;
+
+	for (i = 0; i < _strlen(s); i++)
+	{
+		str[i] = s[_strlen(s) - i];
+	}
+	s = str;
+}
+
+
+/**
+  *_strlen - return the length of a string
+  *@s : the string parameter
+  *
+  *Return: size of a string which is the length of string
+  */
+
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
