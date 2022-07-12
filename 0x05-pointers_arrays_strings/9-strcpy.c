4f0e6404ea9b041aae091c37d5350f5bd1d29349
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-11 19:57:30 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,6 +10,33 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	dest = src;
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
 	return (dest);
 }
+
+/**
+  *_strlen - return the length of a string
+  *@s : the string parameter
+  *
+  *Return: size of a string which is the length of string
+  */
+
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
