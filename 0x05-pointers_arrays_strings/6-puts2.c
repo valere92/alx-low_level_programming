cfb005b91ecba56298c653f56138d66513be6ba6
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-11 18:19:14 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,11 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; i < _strlen(str); i++)
+	for (i = 0; i < _strlen(str); i+2)
 	{
 		_putchar(str[i]);
-		_putchar('\n');
 	}
+	_putchar('\n');
 
 }
 
