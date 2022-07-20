8ec7ada375a2f7703b62f4b5d52873599ee76403
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-20 23:38:25 +0000

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,13 +12,14 @@ void _puts_recursion(char *s)
 {
 	int j = 0;
 
+	_putchar(s[j]);
+
 	if (s[j] == '\0')
 		_putchar('\n');
 
-	_putchar(s[j]);
 	j++;
 
-	if (s[j] != '\0')
+	if (s[j - 1] != '\0')
 		_puts_recursion(&s[j]);
 
 }
