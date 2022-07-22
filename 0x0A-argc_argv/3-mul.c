63b40d38c92d7dbca857652aa509712bd30c419f
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-22 20:42:13 +0000

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
   *main - enter point, print the program name.
@@ -21,35 +22,7 @@ int main(int argc, char *argv[])
 	
 	else 
 	{
-		printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 	return (0);
-}
-
-/**
- * _atoi - Convert a number in a string to an integer
- *
- * @s: String to be converted
- *
- * Return: Integer converted OR (0) if none
- */
-
-int _atoi(char *s)
-{
-	int sign_cache = 1;
-	int result = 0;
-
-	while (s[0] != '\0')
-	{
-		if (s[0] == '-')
-			sign_cache *= -1;
-		else if (s[0] >= '0' && s[0] <= '9')
-			result = (result * 10) + (s[0] - '0');
-		else if (result)
-			break;
-
-		s++;
-	}
-
-	return (result * sign_cache);
 }
