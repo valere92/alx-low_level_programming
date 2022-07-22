666bd3a8f4d839666432eb1d345b21fa3cbbd8a4
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-22 20:46:20 +0000

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,45 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
+
+void print_array_elements(char *str);
 
 /**
-  *main - inter point
-  *@argc : number of arguments
-  *argv : a pointer
-  *
-  *Return: zero as success
-  */
+ * main - Prints all arguments it receives
+ *
+ * @argc: Length of @argv, integer
+ *
+ * @argv: Array of strings of the arguments of this program
+ *
+ * Return: 0, success
+ */
 
 int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+	{
+		print_array_elements(argv[i]);
+		_putchar('\n');
+	}
 
 	return (0);
+}
+
+/**
+ * print_array_elements - Prints all char of a string
+ *
+ * @str: Pointer to string
+ *
+ * Return: void
+ */
+
+void print_array_elements(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 }
