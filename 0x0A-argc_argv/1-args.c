45dd21f82523ee8bdf5c4fecbab220d53a3fcfd0
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-22 18:16:34 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *main - inter point, print arguments number
+  *@argc : number of arguments
+  *@argv : pointer to pointer to array of arguments
+  *
+  *Return: 0 as success
+  */
+
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
