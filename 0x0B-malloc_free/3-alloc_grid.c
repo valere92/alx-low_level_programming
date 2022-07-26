1db251a1c3f05c82c2496e9f221b6569704e9d47
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-26 01:28:07 +0000

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *alloc_grid - create a 2D array
+  *@width : the number of columns
+  *@height : the number of lines
+  *
+  *Return: a pointer to the 2D array
+  */
+
+int **alloc_grid(int width, int height)
+{
+	char **p;
+	char *s;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	p = malloc(sizeof(char) * width);
+	s = malloc(sizeof(char) * height);
+
+	if (p == NULL || s == NULL)
+		return (NULL);
+
+	for (i = 0; i < width; i++)
+		for (j = 0; j < height; j++)
+		{
+			s[j] = 0;
+			p[i] = &s[j];
+		}
+
+	return (p)
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -5,3 +5,5 @@
 *Returns a pointer to the array, or NULL if it fails
 
 1-strdup.c :  a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+
+3-alloc_grid.c : a function that returns a pointer to a 2 dimensional array of integers.
