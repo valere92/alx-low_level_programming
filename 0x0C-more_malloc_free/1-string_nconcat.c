3f456b4991a11362f97f10a3b73edd34ebf92696
valere92 <romualdvalerendiguintamack@gmail.com>
2022-07-28 09:11:00 +0000

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *string_nconcat - concatenate two strings
+  *@s1 : the first string
+  *@s2 : the second string
+  *@n : amount of byte to concatenate on s2
+  *
+  *Return: a pointer to the newly alocated space
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int *s;
+	int l;
+
+	l = strlen(s1) + n;
+
+	s = malloc(l + 1);
+
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+	for (i = 0; i < strlen(s1); i++)
+		s[i] = *(s1 + i);
+
+	for (j = 0; j < n; j++; i++)
+		s[i + 1] = *(s2 + j);
+
+	s[l + 1] = '\0';
+
+	return (s);
+}
+
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,1 +1,5 @@
 0-maloc_checked.c : a function that alocates memory using malloc if function fails, cause normal process termination with a status value of 98
+
+1-string_nconcat.c : a function that concatenates two strings.
+
+2-calloc.c : a function that allocates memory for an array, using malloc.
