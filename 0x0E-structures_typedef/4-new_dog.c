51819713f749f411796293bd1194e7a17de732e3
valere92 <romualdvalerendiguintamack@gmail.com>
2022-08-01 18:52:03 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,9 +20,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	(*p).name = name;
-	(*p).age = age;
-	(*p).owner = owner;
+	(*new_dog).name = name;
+	(*new_dog).age = age;
+	(*new_dog).owner = owner;
 
 	return (new_dog);
 }
