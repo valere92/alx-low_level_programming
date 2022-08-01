5756007a5677d73fe7d9bbfd43215bd2524695ec
valere92 <romualdvalerendiguintamack@gmail.com>
2022-08-01 20:32:13 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,53 +1,75 @@
-#include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "dog.h"
+
+int _strLen(char *str);
+void fillMem(char *str, int strLen, char *dest);
 
 /**
-  *new_dog - create a new dog
-  *@name : the name
-  *@age : age
-  *@owner : proprior
-  *
-  *Return: a pointer to new_dog struct
-  */
+ * new_dog - Creates a new dog
+ *
+ * @name: Name of dog
+ *
+ * @age: Age of dog
+ *
+ * @owner: Owner of dog
+ *
+ * Return: Pointer to the newly created dog (SUCCESS) or
+ * NULL if insufficient memory was available (FAILURE)
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	unsigned long int i, j;
+	dog_t *n_dog;
+	int nameLen, ownerLen;
 
-	new_dog = malloc(sizeof(dog_t));
+	n_dog = malloc(sizeof(dog_t));
 
-	if (new_dog == NULL)
+	if (n_dog == NULL)
 		return (NULL);
 
-	(*new_dog).name = malloc(sizeof(strlen(name) + 1));
+	nameLen = _strLen(name);
+	n_dog->name = malloc(sizeof(char) * nameLen + 1);
 
-	if ((*new_dog).name == NULL)
+	if (n_dog->name == NULL)
 	{
-		free(new_dog);
+		free(n_dog);
 		return (NULL);
 	}
 
-	(*new_dog).owner = malloc(sizeof(strlen(owner) + 1));
+	fillMem(name, nameLen, n_dog->name);
 
-	if ((*new_dog).owner == NULL)
+	ownerLen = _strLen(owner);
+	n_dog->owner = malloc(sizeof(char) * ownerLen + 1);
+
+	if (n_dog->owner == NULL)
 	{
-		free(new_dog);
+		free(n_dog);
+		free(n_dog->name);
 		return (NULL);
 	}
 
-	for (i = 0; i < strlen(name); i++)
-		new_dog->name[i] = name[i];
+	fillMem(owner, ownerLen, n_dog->owner);
 
-	(*new_dog).age = age;
+	n_dog->age = age;
 
-	for (j = 0; j < strlen(owner); j++)
-		new_dog->owner[j] = owner[j];
-
-	new_dog->name[i] = '\0';
-	new_dog->owner[j] = '\0';
-
-	return (new_dog);
+	return (n_dog);
 }
+
+/**
+ * _strLen - Get length of a string
+ *
+ * @str: A string
+ *
+ * Return: Length of string
+ */
+
+int _strLen(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+
+	return (i);
+}
+
