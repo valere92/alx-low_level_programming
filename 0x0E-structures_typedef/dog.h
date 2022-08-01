39a33bbb8cb52d39488a85e4825e1b2f46f3c07f
valere92 <romualdvalerendiguintamack@gmail.com>
2022-08-01 11:40:43 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-struct dog {
+/**
+  *struct dog - create a new type of data dog
+  *@name : the dog name
+  *@age : the age of dog
+  *@owner : the dog proprior
+  *
+  *Description: define a dog identifiers
+  */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
