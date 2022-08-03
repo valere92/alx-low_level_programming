0ee04ac07bccdee2da4095aa4b7ac43b659aa2fd
valere92 <romualdvalerendiguintamack@gmail.com>
2022-08-03 11:06:39 +0000

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,29 +2,7 @@
 #include <stdio.h>
 
 /**
-  *f - a function that print nanme
-  *@name : the name to print
-  *
-  *Return: void
-  */
-
-void f(char *name)
-{
-	int l = 0;
-	int i, j = 0;
-
-	while (name[j] != '\0')
-		l++;
-
-	for (i = 0; i < l; i++)
-		_putchar(name[i]);
-	
-	_putchar('\n');
-
-}
-
-/**
-  *print_name - function that print name
+  *@print_name - function that print a name
   *@name : the name to print
   *@f : the pointerto f
   *
@@ -33,6 +11,8 @@ void f(char *name)
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 
 	(*f)(name);
 
