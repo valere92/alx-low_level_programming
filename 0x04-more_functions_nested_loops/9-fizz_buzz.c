974269501b63bd8437b16a58284ddb0f5fdb0d16
valere92 <romualdvalerendiguintamack@gmail.com>
2023-03-23 19:55:44 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 99; i++)
 	{
 
 		if (i % 3 == 0 && i % 5 != 0)
@@ -19,10 +19,11 @@ int main(void)
 		else if (i % 5 == 0 && i % 3 != 0)
 			printf("Buzz ");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
 	}
+	printf("Buzz");
 	printf("\n");
 
 	return (0);
