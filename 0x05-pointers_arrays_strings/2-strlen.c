1f867c34f55d625648284a83e66eda29ce6053ab
valere92 <romualdvalerendiguintamack@gmail.com>
2023-03-27 08:28:11 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * _strlen - find the length of a string
+ * @s: the string
+ *
+ * Return: the length of the sting
+ */
+
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
