c8f5fc4379ea326e308edb9671fa6503c9c9fee3
valere92 <romualdvalerendiguintamack@gmail.com>
2023-03-27 10:23:47 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string reverse
+ * @s: the string to print
+ *
+ * Return: no return value
+ */
+
+
+
+void print_rev(char *s)
+{
+	int len = _strlen(s);
+
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+
+	_putchar('\n');
+}
+
+/**
+ * _strlen - find the length of a string
+ * @s: the string
+ *
+ * Return: the length of the sting
+ */
+
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
